d3c0a0def1cc90a1630d6856bfc955f47b3cac36
frankgrimy <f.gonzalez.12@ug.uchile.cl>
2023-01-22 12:57:03 -0300

    Optimized imports

--- Novation Launchkey Mini MK3/panvol.py
+++ Novation Launchkey Mini MK3/panvol.py
@@ -1,10 +1,8 @@
 ### Panning and volume controls for Mixer and Channel Rack scenes.
-import math
-import specialmath as math2
-import mixer
-import channels
-import ui
-
+from specialmath import truncate as trnc
+from mixer import setTrackPan, setTrackVolume
+from channels import setChannelPan, setChannelVolume
+from ui import setHintMsg
 
 class PanVol:
     def __init__(self, data1, data2, pos, count, scenetype, *knobmap):
@@ -20,23 +18,23 @@ class PanVol:
             if self.data1 == self.knobmap[x]:
                 if self.scenetype == "Mixer":
                     if self.pos+y-21 < self.count-1:
-                        mixer.setTrackPan(*(self.pos+y-21, math2.truncate((self.data2*2/127)-1 , 2)))
+                        setTrackPan(*(self.pos+y-21, trnc((self.data2*2/127)-1 , 2)))
                         return 1
                     else:
                         if self.pos+y-21 == self.count-1:
-                            ui.setHintMsg("You can't pan the Current insert")
+                            setHintMsg("You can't pan the Current insert")
                             return 0
                         else:
-                            ui.setHintMsg("The insert you're trying to pan doesn't exist!")
+                            setHintMsg("The insert you're trying to pan doesn't exist!")
                             return 0
                         
 
                 elif self.scenetype == "Channel rack":
                     if self.pos+y-21 < self.count:
-                        channels.setChannelPan(*(self.pos+y-21, math2.truncate((self.data2*2/127)-1 , 2)))
+                        setChannelPan(*(self.pos+y-21, trnc((self.data2*2/127)-1 , 2)))
                         return 1
                     else:
-                        ui.setHintMsg("The channel you're trying to pan doesn't exist!")
+                        setHintMsg("The channel you're trying to pan doesn't exist!")
                         return 0
     
     def Vol(self):
@@ -44,20 +42,20 @@ class PanVol:
             if self.data1 == self.knobmap[x]:
                 if self.scenetype == "Mixer":
                     if self.pos+y-21 < self.count-1:
-                        mixer.setTrackVolume(*(self.pos+y-21, (self.data2/127)*0.8))
+                        setTrackVolume(*(self.pos+y-21, (self.data2/127)*0.8))
                         return 1
                     else:
                         if self.pos+y-21 == self.count-1:
-                            ui.setHintMsg("You can't level the Current insert")
+                            setHintMsg("You can't level the Current insert")
                             return 0
                         else: 
-                            ui.setHintMsg("The insert you're trying to level doesn't exist!")
+                            setHintMsg("The insert you're trying to level doesn't exist!")
                             return 0
                 
                 elif self.scenetype == "Channel rack":
                     if self.pos+y-21 < self.count:
-                        channels.setChannelVolume(*(self.pos+y-21, self.data2*0.78125/127))
+                        setChannelVolume(*(self.pos+y-21, self.data2*0.78125/127))
                         return 1
                     else:
-                        ui.setHintMsg("The channel you're trying to level doesn't exist!")
+                        setHintMsg("The channel you're trying to level doesn't exist!")
                         return 0
