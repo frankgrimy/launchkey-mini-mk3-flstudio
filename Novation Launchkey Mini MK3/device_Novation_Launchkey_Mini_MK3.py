fa1316001899e802a5d3207bbbc9e0eb30a519c9
frankgrimy <f.gonzalez.12@ug.uchile.cl>
2023-02-22 14:42:58 -0300

    Added reverse cycling by long pressing the Up button

--- Novation Launchkey Mini MK3/device_Novation_Launchkey_Mini_MK3.py
+++ Novation Launchkey Mini MK3/device_Novation_Launchkey_Mini_MK3.py
@@ -1,75 +1,45 @@
-#   name=Novation Launchkey Mini MK3 (by Frank Grimy) v1.3.1
+#   name=Novation Launchkey Mini MK3 (by Frank Grimy) v1.4.0
 #   url=https://forum.image-line.com/viewtopic.php?f=1994&t=260133
 #   supportedDevices=MIDIIN2 (Launchkey Mini MK3 MID,Launchkey Mini MK3 DAW Port
 
 # Import list
-import midi, device, mixer, general, ui, channels
+import midi, mixer, channels
 from mxr.mirectangle import Rectangle
-import pads, knobs, mixerlowpads, sceneup, browse, handlers
+import pads, knobs, mixerlowpads, browse, handlers
 import variables as var, constants as cons, drumpads as drum, stepsequencer as step, panvol as pv, scenedown as scdown
-import uimessages as uimsg, scenedownlights as downlight, upperpadslights as uplights, initrefresh as initrfsh, padknobmodes as pkm
+import uimessages as uimsg, scenedownlights as downlight, upperpadslights as uplights, padknobmodes as pkm
 import upperpadsactions as upact, crlowpads as crlp, transportbuttons as trns
-#from cust_gen import vital, pigments3, surge
 from cust_gen import *
 from fruity_gen import *
 
-#import pl.controls as plctrl
 import mxr.embdeq
 from editors.shortcuts import *
 from time import time
+from deinit import QuitFL
+from initialize import Startup
+from windowfocus import WindowFocus
+from sceneup import SceneUpMidi, UpColor, UpColorsIdle
 
 apiver4script=23 # Scripting API version compatible with this script.
 
+def OnInit():
+    Startup.OnInit()
 
-def OnInit():           # Set up "DAW mode" in the controller after linking the script to the controller. RTFM to know why is this important!
-    if general.getVersion() >= apiver4script:
-        initrfsh.Initialize() # Initialize the controller.
-        cons.startTime = time() # Sets the start time for the script.
-    else:
-        print("")
-        print ("This script might not be supported in this FL Studio version.")
-        print ("Please upgrade to version 20.9.2 (build 2963 for Windows, or MacOS equivalent) or higher."), print("")
-        print("Scripting API version needed:", end=" "), print(apiver4script)
-        print("Scripting API version installed:", end=" "), print(general.getVersion())
-        print("")
-        print("Attempting to start anyway...")
-        initrfsh.Initialize() # Initialize the controller.
-    
-    if device.isAssigned():
-        device.midiOutMsg(0xBF, 0, 0x09, 0x01) # Sets the default knob target to "Volume" in the controller.
-
-
-def OnDeInit():         # Gets back to Standalone mode after exiting FL Studio or unloading the script.
-    if device.isAssigned():
-        device.midiOutMsg(0xBF, 0, 0x73, 0x00) # Don't know what this does, but it's probably to close the connection gracefully (probably useful for Linux/Wine, since it seems the script used to crash FL Studio without this).
-        device.midiOutMsg(159,0,12,0) # Gets back to standalone mode.
-        print("Connection disabled. Back to Standalone mode.\n")
+def OnDeInit():
+    QuitFL.OnDeInit()
 
 
 def OnIdle(): # Functionality that runs in the background. This lets the script to give feedback to the user in real time and in context.
+    UpColor
     uplights.Padlights() # Upper pads + transport lighting.
     var.currentTime = time() # Sets the current time for the script.
-    
-    ### Set the focused window to a variable
-    if ui.getFocused(0):
-        var.FOCUS_STATUS = "Mixer"
-    elif ui.getFocused(1):
-        var.FOCUS_STATUS = "Channel rack"
-    elif ui.getFocused(2):
-        var.FOCUS_STATUS = "Playlist"
-    elif ui.getFocused(3):
-        var.FOCUS_STATUS = "Piano roll"
-    elif ui.getFocused(4):
-        var.FOCUS_STATUS = "Browser"
-    elif ui.getFocused(5):
-        var.FOCUS_STATUS = "Plugin"
+    WindowFocus() # Detects the focused window in FL Studio.
     
 ### Colors for Scene Down button
-    if not var.SHIFT_STATUS:
-        downlight.Downbutton()
+    downlight.Downbutton()
 
 ### Colors for Scene-up button
-    sceneup.UpColorsIdle()
+    UpColorsIdle()
 
 ### Lower pads colors for Mixer mode!
     mixerlowpads.LowerLights()
@@ -78,17 +48,19 @@ def OnIdle(): # Functionality that runs in the background. This lets the script 
     crlp.LowerLights()
 
 ### Step-sequencer lights
-    #step.SeqLights()
+    step.SeqLights()
 
 ### Playlist track mute state
     #plctrl.MuteLights()
     
-    ShortLights() # Shortcuts lights for Editor mode.
+    ShortLights() # Lights for shortcuts in Editor mode.
     peakMonitor() # Peak meter monitor (Editor mode).
 
 
 def OnMidiMsg(event): # Functionality to integrate buttons, pads and knobs operations (CC). This data is passed to FL Studio, and it does stuff with it.
-    sceneup.UpPushed(event.midiId, event.data1, event.data2) # Scene-up button behavior.
+    #UpMidi.OnIdle(event.midiId, event.data1, event.data2) # Scene-up button behavior.
+    #SceneUpMidi.OnIdle(event.midiId, event.data1, event.data2) # Scene-up button behavior.
+    SceneUpMidi(event, 0.15)
     trns.TrBehavior(event.midiId, event.data1, event.data2) # Transport buttons behavior.
     browse.Arrowkeys(event.midiId, event.data1, event.data2) # Arrow keys behavior (Shift + Arrow buttons).
     
@@ -180,15 +152,10 @@ def OnControlChange(scene):
                     channel.Vol()
       
     scene.handled = handlers.SceneHandler(scene.midiId, scene.data1) # If a CC is integrated, then FL Studio will show a yellow icon.
-    sceneup.UpBehavior(scene.midiId, scene.data1, scene.data2) # Up button behavior.
-    sceneup.UpColorsPush(scene.midiId, scene.data1) # Up button colors.
+    #sceneup.UpBehavior(scene.midiId, scene.data1, scene.data2) # Up button behavior.
+    #sceneup.UpColorsPush(scene.midiId, scene.data1) # Up button colors.
 
     # Mixer rectangle indicator.
     if var.SCENE_SEL == "Mixer":
         if scene.data1 and scene.data2:
             Rectangle()
-
-# def OnRefresh(HW_Dirty_Mixer_Controls):
-#     # trackx = [number, name, volume, pan, mute, arm, fxStatus]
-#     # You can query the status of any mixer track by calling Insert(track number).property    
-
